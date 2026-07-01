fb6f0c2d0876ec99832047ee638a62ff4a4e426d
Pakala siddartha <siddartha.pakala12@gmail.com>
2026-07-01 21:28:41 +0530

    fix DatePicker input padding to resolve overlap

--- client/src/pages/StatisticsPage.jsx
+++ client/src/pages/StatisticsPage.jsx
@@ -55,7 +55,7 @@ function DatePicker({ value, onChange }) {
     <div className="relative flex-1 sm:flex-initial">
       <div ref={triggerRef} onClick={openCalendar} className="relative flex items-center cursor-pointer select-none">
         <input type="text" readOnly value={formatDisplayDate(value)}
-          className="w-full sm:w-36 md:w-40 bg-[#f5f5f7] border border-black/10 rounded-xl px-4 py-2 text-xs font-semibold text-gray-800 focus:outline-none focus:bg-white focus:border-orange-400 transition-all duration-200 cursor-pointer hover:border-black/20"
+          className="w-full sm:w-36 md:w-40 bg-[#f5f5f7] border border-black/10 rounded-xl px-4 py-2 pl-10 text-xs font-semibold text-gray-800 focus:outline-none focus:bg-white focus:border-orange-400 transition-all duration-200 cursor-pointer hover:border-black/20"
         />
         <Calendar size={14} className="absolute left-3.5 text-orange-500 pointer-events-none" />
       </div>
